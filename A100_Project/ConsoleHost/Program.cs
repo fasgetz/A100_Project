ea11bf3ab4d1ec4783aea8ca96614140bdb01ded
Andrew Andrew <fasgetz@hotmail.com>
2019-05-29 15:19:08 +0300

    Console server add

---
+++ A100_Project/ConsoleHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(A100_Service.Service1)))
+            {
+                host.Open(); // Открываем хост
+                Console.WriteLine("Хост открыт");
+                
+                Console.ReadKey(); // Заглушка
+            }
+        }
+    }
+}
