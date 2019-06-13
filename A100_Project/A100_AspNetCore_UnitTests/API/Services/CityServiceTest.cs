b826d793f81b83ba647055f8f1035283b1e0b1a8
Andrew Andrew <fasgetz@hotmail.com>
2019-06-13 13:31:23 +0300

    Add unit testing

---
+++ A100_Project/A100_AspNetCore_UnitTests/API/Services/CityServiceTest.cs
@@ -0,0 +1,120 @@
+﻿using A100_AspNetCore.API.Controllers;
+using A100_AspNetCore.Models.A100_Models.DataBase;
+using A100_AspNetCore.Services.API.CityService;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace A100_AspNetCore_UnitTests.API.Services
+{
+
+    /// <summary>
+    /// Класс, который проверяет работоспособность сервиса City
+    /// </summary>
+
+    public class CityServiceTest
+    {
+        private ICityService service; // Сервис VIK
+
+        // Объявления в конструкторе
+        public CityServiceTest()
+        {
+            // Arrange
+
+            service = new CityService();
+        }
+
+        // Тестовый метод, который получает города, переданные в параметрах
+        [Theory]
+        [InlineData("Москва")]
+        [InlineData("Владимир")]
+        [InlineData("Тула")]
+        [InlineData("Калуга")]
+        [InlineData("Балашиха")]
+        public async void GetCityName(string name)
+        {
+            var city = await service.GetCityName(name);
+            
+            Assert.NotNull(city); // Проверяем, получин ли объект
+            Assert.Equal(name, city.CityName); // Проверяем на соответствие
+        }
+
+
+        // Тестовый метод, который получает весь список городов
+        [Fact]
+        public async void GetAllCities()
+        {
+            var list = await service.GetAllCityes();
+
+            Assert.NotNull(list); // Проверяем список на пустоту (Если не пустой, то города получены)
+        }
+
+
+        // Тестовый метод, который не получит не существующий город
+        [Theory]
+        [InlineData("Какой-то там город")]
+        [InlineData("Владик")]
+        [InlineData("Масква")]
+        [InlineData("Moscow")]
+        [InlineData("Царь-Град")]
+        public async void GetFailedCityName(string name)
+        {
+            var city = await service.GetCityName(name);
+
+            Assert.Null(city);
+        }
+
+        // Метод, который получает город по айди (успешный)
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3053)]
+        [InlineData(6)]
+        [InlineData(1009)]
+        [InlineData(8)]
+        public async void GetCityIdTrue(int id)
+        {
+            var city = await service.GetCityId(id);
+
+            Assert.NotNull(city);
+        }
+
+        // Получение городов по айди, которых не существует
+        [Theory]
+        [InlineData(155)]
+        [InlineData(30532)]
+        [InlineData(65532)]
+        [InlineData(3241009)]
+        [InlineData(423428)]
+        public async void GetCityIdFalse(int id)
+        {
+            var city = await service.GetCityId(id);
+
+            Assert.Null(city);
+        }
+
+
+
+
+        //// Тестовый метод контроллера с созданием фейкового объекта
+        //[Fact]
+        //public async Task tested()
+        //{
+        //    var mock = new Mock<ICityService>();
+        //    mock.Setup(i => i.GetCityName(It.IsAny<string>())).ReturnsAsync(new City { CityName = "Moscow" });
+
+        //    var controller = new CityController(mock.Object);
+
+        //    var result = await controller.GetCityName(new City { CityName = "Тест"});
+
+
+        //    Assert.NotNull(result);
+
+        //}
+
+
+
+    }
+}
---
+++ A100_Project/A100_AspNetCore_UnitTests/API/Services/VikServiceTest.cs
@@ -0,0 +1,82 @@
+﻿using A100_AspNetCore.API.Authentication.AuthenticationControllers;
+using A100_AspNetCore.API.Controllers;
+using A100_AspNetCore.Models.A100_Models.DataBase;
+using A100_AspNetCore.Models.API;
+using A100_AspNetCore.Models.Authentication;
+using A100_AspNetCore.Services.API;
+using A100_AspNetCore.Services.API.VikService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace A100_AspNetCore_UnitTests.API.Services
+{
+
+    /// <summary>
+    /// Класс, который проверяет работоспособность сервиса VIK
+    /// </summary>
+
+
+    public class VikServiceTest
+    {
+
+        private IVikService service; // Сервис VIK
+
+
+        // Объявления в конструкторе
+        public VikServiceTest()
+        {
+            // Arrange
+
+            var services = new ServiceCollection();
+            services.AddTransient<IVikService, VikService>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            service = serviceProvider.GetService<IVikService>();
+        }
+
+
+        [Fact]
+        public async void TestedGetVikNamesMethod()
+        {
+            // Act
+            var test = await service.GetVikNames();
+
+
+
+            // Assert
+
+            Assert.IsType<List<VikElement>>(test);
+
+            Assert.NotNull(test); // Проверяем, что не пустой
+
+            // Проверяем количество, чтобы было 19 элементов (можно убрать, если будет дополняться)
+            Assert.Equal(19, test.Count); 
+        }
+
+
+
+        // Тестовый метод, который получает VIK по ResoultID
+        [Theory]
+        [InlineData(11998)]
+        [InlineData(9057)]
+        public async void GetVikByResoultID(int ResoultID)
+        {
+            // Act
+            var test = await service.GetVik(ResoultID); // получаем VIK
+
+
+            // Assert
+            Assert.NotNull(test); // Проверяем, что не пустой и объекты получены            
+
+        }
+
+
+    }
+}
