9f741fa8bac90b78d3167df03acb0f7cab525b4b
Andrew Andrew <fasgetz@hotmail.com>
2019-06-18 10:51:08 +0300

    Add comments

--- A100_Project/A100_AspNetCore/Controllers/AccountController.cs
+++ A100_Project/A100_AspNetCore/Controllers/AccountController.cs
@@ -137,13 +137,14 @@ namespace A100_AspNetCore.Controllers
             }
         }
 
+        /// <summary>
+        /// Метож выхода из аккаунта
+        /// </summary>
+        /// <returns>Редиректит на главную страницу контроллера Home</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> LogOff()
         {
-            // Ветвь девелопментс
-
-
             // Удаляем аутентификационные куки и делаем редиректинг на главную сраницу 
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
