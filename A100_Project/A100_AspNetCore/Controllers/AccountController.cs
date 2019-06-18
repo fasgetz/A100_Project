40927a05436a098311b9cb3f8c8b596a8299f5d0
Andrew Andrew <fasgetz@hotmail.com>
2019-06-18 10:48:18 +0300

    Develop branch

--- A100_Project/A100_AspNetCore/Controllers/AccountController.cs
+++ A100_Project/A100_AspNetCore/Controllers/AccountController.cs
@@ -141,7 +141,10 @@ namespace A100_AspNetCore.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> LogOff()
         {
-            // Удаляем аутентификационные куки и делаем редиректинг на главную сраницу
+            // Ветвь девелопментс
+
+
+            // Удаляем аутентификационные куки и делаем редиректинг на главную сраницу 
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
